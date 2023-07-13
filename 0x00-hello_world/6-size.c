5d8512ffea2bbada03f15494d6296c269648fdb0
fahad-moha <khiladifahad02@gmail.com>
2023-07-13 06:57:28 -0700

    6-size.c updated

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("char size =>%d\n"),sizeof(char);
+	printf("float size =>%d\n"),sizeof(float);
+	printf("long int size =>%d\n"),sizeof(long int);
+	printf("int size =>%d\n"),sizeof(int);
+	return (0);
+}
