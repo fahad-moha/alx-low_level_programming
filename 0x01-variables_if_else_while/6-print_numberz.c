45f368ff31349d627c7be509406173470b9df2ec
fahad-moha <khiladifahad02@gmail.com>
2023-07-14 12:43:04 -0700

    6-print_numberz.c updated

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - English alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char nmber[10] = "0123456789";
+        int i;
+
+        for (i = 0; i < 10; i++)
+        {
+                putchar( number[i]);
+        }
+        putchar("\n");
+        return (0);
+}
