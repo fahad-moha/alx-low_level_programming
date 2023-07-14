1ad56c41d5e35db4647d29b4ebcb32ce8ef1ed09
fahad-moha <khiladifahad02@gmail.com>
2023-07-14 10:01:47 -0700

    2-print_alphabet.c updated

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - English alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char english_alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(english_alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
