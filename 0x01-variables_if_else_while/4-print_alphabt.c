e9b7ad1afd6c1c4432105e9a2adcc3577ee671eb
fahad-moha <khiladifahad02@gmail.com>
2023-07-14 11:53:11 -0700

    4-print_alphabt.c updated

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - English alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (letter[i] == 'q' || letter[i] == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(letter[i]);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
