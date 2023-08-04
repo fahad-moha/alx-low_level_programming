a421759ee006a7bed9499b3edfed6104e925124b
fahad-moha <khiladifahad02@gmail.com>
2023-08-04 11:55:28 -0700

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,9 @@
 
 /**
   * main - prints its name.
-  * @*argv[]: the argv array that is passed to the main function
-  * @argc __attribute__((unused)): to indicate that the
-  * argc parameter is intentionally unused
+  * @argv: the argv array that is passed to the main function
+  * @argc: is an integer that represents the number of
+  * command-line arguments passed to the program
   * Return: Always 0 (success)
   */
 int main(int argc __attribute__((unused)), char *argv[])
