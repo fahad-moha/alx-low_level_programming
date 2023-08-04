886ddfb05b13b93d0442025c2e371a234a7b1f69
fahad-moha <khiladifahad02@gmail.com>
2023-08-04 11:01:24 -0700

    0-whatsmyname.c updated

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - prints its name.
+  * @*argv[]: the argv array that is passed to the main function
+  * @argc __attribute__((unused)): to indicate that the
+  * argc parameter is intentionally unused
+  * Return: Always 0 (success)
+  */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
