c41de4dd3411f6a18a7ac8b9c5b0e549b60d04e8
fahad-moha <khiladifahad02@gmail.com>
2023-08-06 23:47:15 -0700

    0-create_array.c updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,12 +15,12 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	array = (char *)malloc((size + 1) * sizeof(char));
+
+	array = (char *)malloc(size * sizeof(char));
 	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 		array[i] = c;
-	array[size] = '\0';
 	return (array);
 }
