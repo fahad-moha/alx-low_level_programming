334ea701cb6547de99db6e871a54a08032b1056a
fahad-moha <khiladifahad02@gmail.com>
2023-08-10 08:03:03 -0700

    0-malloc_checked.c updated

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,10 +8,13 @@
   */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	void *ptr;
+
+	ptr = malloc(b);
+
 	if (ptr == NULL)
-	{
-		return ((void *)98);
-	}
+		exit(98);
+
 	return (ptr);
 }
+
