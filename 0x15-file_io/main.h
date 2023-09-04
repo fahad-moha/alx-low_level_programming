07d77f7048a7b0de855050cfaa67b37f30337ae3
fahad-moha <khiladifahad02@gmail.com>
2023-09-04 11:54:25 -0700

    updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,4 +14,20 @@ int main(int argc, char **argv);
 int copy_file(const char *source, const char *destination);
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
+
+#define ERROR_INVALID_ARGUMENTS "Invalid number of arguments. Usage: elf_header elf_filename"
+#define ERROR_OPEN_FILE "Failed to open the file."
+#define ERROR_READ_HEADER "Failed to read the ELF header."
+#define ERROR_NOT_ELF_FILE "Not an ELF file."
+
+void print_error(const char *message);
+void print_elf_header_info(const Elf64_Ehdr *header);
+
 #endif
