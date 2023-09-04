3fde6c37de54d2ff94e81bedbbacb1948e0dbd02
fahad-moha <khiladifahad02@gmail.com>
2023-09-04 04:31:01 -0700

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,21 @@
+#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <stdio.h>
+#include "main.h"
 
 #define BUFFER_SIZE 1024
+
+/**
+ * print_error - Prints an error message to the POSIX standard error.
+ * @message: The error message to print.
+ */
+void print_error(const char *message)
+{
+	dprintf(STDERR_FILENO, "%s\n", message);
+}
 
 /**
  * main - Entry point of the program.
@@ -22,29 +32,51 @@ int main(int argc, char **argv)
 	mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
-		exit(dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n") || 97);
+	{
+		print_error("Usage: cp file_from file_to");
+		exit(97);
+	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		exit(dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-					argv[1]) || 98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, permissions);
 	if (fd_to == -1)
-		exit(dprintf(STDERR_FILENO, "Error: Can't write to file %s\n",
-					argv[2]) || (close(fd_from), 99));
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		close(fd_from);
+		exit(99);
+	}
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
-			exit(dprintf(STDERR_FILENO, "Error: Can't write to file %s\n",
-						argv[2]) || (close(fd_from), close(fd_to), 99));
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
 	}
 	if (bytes_read == -1)
-		exit(dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-					argv[1]) || (close(fd_from), close(fd_to), 98));
-	exit((close(fd_from) == -1 &&
-				(dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
-						fd_from) || 100)) ||
-			(close(fd_to) == -1 && (dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
-							fd_to) || 100)));
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
 	return (0);
 }
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,4 +9,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+void print_error(const char *message);
+int main(int argc, char **argv);
+
 #endif
